9ad1d854f43d5dc225b2d0fdf11c53c96d14bf6e
Marc Cooperstein <mcooperstein@gmail.com>
2017-02-01 19:02:08 -0800

    Changed init location and added input field and button for changing location

--- scripts.js
+++ scripts.js
@@ -4,8 +4,8 @@ var canvas = document.getElementById("map-canvas");
 
 function initMap() {
     var mapOptions = {
-        center: new google.maps.LatLng(-34.397, 150.644),
-        zoom: 5
+        center: new google.maps.LatLng(36.778, -119.417),
+        zoom: 6
     }
     map = new google.maps.Map(canvas, mapOptions);
 };
