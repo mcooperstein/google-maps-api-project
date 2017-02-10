b498a00ed4d7ae75b28a0562cfc9aabe0d520356
Marc Cooperstein <mcooperstein@gmail.com>
2017-02-09 19:13:29 -0800

    added search functionality

--- scripts.js
+++ scripts.js
@@ -8,4 +8,39 @@ function initMap() {
         zoom: 6
     }
     map = new google.maps.Map(canvas, mapOptions);
+    var input = document.getElementById("user-input");
+
+    var autocomplete = new google.maps.places.Autocomplete(input, {
+        placeIdOnly: true
+    });
+    autocomplete.bindTo("bounds", map);
+    var geocoder = new google.maps.Geocoder;
+    var marker = new google.maps.Marker({
+        map: map
+    });
+
+    autocomplete.addListener('place_changed', function () {
+        var place = autocomplete.getPlace();
+
+        if (!place.place_id) {
+            return;
+        }
+        geocoder.geocode({
+            'placeId': place.place_id
+        }, function (results, status) {
+
+            if (status !== 'OK') {
+                window.alert('Geocoder failed due to: ' + status);
+                return;
+            }
+            map.setZoom(11);
+            map.setCenter(results[0].geometry.location);
+            // Set the position of the marker using the place ID and location.
+            marker.setPlace({
+                placeId: place.place_id,
+                location: results[0].geometry.location
+            });
+            marker.setVisible(true);
+        });
+    });
 };
