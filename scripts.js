2ed4ac92ba1545f0fc895b1e4cb6dd26b31356a2
Marc Cooperstein <mcooperstein@gmail.com>
2017-02-22 21:41:34 -0800

    removed some comments, added link to jquery

--- scripts.js
+++ scripts.js
@@ -1,54 +1,4 @@
 //API Key: AIzaSyCjsuf6lDKHz_fkTGZTNuh3qehl_LzJP-o
-/*var map;
-var canvas = document.getElementById("map-canvas");
-
-function initMap() {
-    var mapOptions = {
-        center: new google.maps.LatLng(36.778, -119.417),
-        zoom: 6
-    }
-    map = new google.maps.Map(canvas, mapOptions);
-    var input = document.getElementById("user-input");
-
-    var autocomplete = new google.maps.places.Autocomplete(input, {
-        placeIdOnly: true
-    });
-    autocomplete.bindTo("bounds", map);
-    var geocoder = new google.maps.Geocoder;
-    var marker = new google.maps.Marker({
-        map: map
-    });
-
-    autocomplete.addListener('place_changed', function () {
-        var place = autocomplete.getPlace();
-
-        if (!place.place_id) {
-            return;
-        }
-        geocoder.geocode({
-            'placeId': place.place_id
-        }, function (results, status) {
-
-            if (status !== 'OK') {
-                window.alert('Geocoder failed due to: ' + status);
-                return;
-            }
-            map.setZoom(11);
-            map.setCenter(results[0].geometry.location);
-            // Set the position of the marker using the place ID and location.
-            marker.setPlace({
-                placeId: place.place_id,
-                location: results[0].geometry.location
-            });
-            marker.setVisible(true);
-        });
-    });
-};
-*/
-
-/*$(document).ready(function() {
-  initMap();
-});*/
 
 var panorama;
 var map;
@@ -144,11 +94,11 @@ function geocodeAddress(geocoder, resultsMap, showPanorama) {
 
 }
 
-function getRandomInt(min, max) {
+/*function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min)) + min;
-}
+}*/
 
 function getCircle(magnitude) {
     return {
